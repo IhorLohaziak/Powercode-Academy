832f8339096be42bcfcbb92adf8280717ea298cd
IhorLohaziak <lordlir@ukr.net>
2022-07-30 13:26:42 +0300

    2 commit

--- Frontend-Advanced/loader/script.js
+++ Frontend-Advanced/loader/script.js
@@ -1,6 +1,9 @@
 function download() {
   const xhr = new XMLHttpRequest();
-  xhr.open("get", "are.mp4");
+  xhr.open(
+    "get",
+    "НАЗВА БУДЬ-ЯКОГО МП4 файла, який потрібно помістити в корінь проекту, чим більший за розміром тим краще"
+  );
   xhr.onprogress = function ({ loaded }) {
     const max = (+xhr.getResponseHeader("content-length") / 1000000).toFixed(0);
     const current = (loaded / 1000000).toFixed(0);
